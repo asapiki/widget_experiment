3326bb972f438544cb5f9419c2a83cead6bfbf29
asapiki <aiki.asap@gmail.com>
2019-11-21 02:33:23 +0900

    AppBar.centerTitleを実験できる

--- lib/pages/app_bar_lab_page.dart
+++ lib/pages/app_bar_lab_page.dart
@@ -9,42 +9,75 @@ class AppBarLabPage extends LabPage {
 }
 
 class _AppBarLabPageState extends State<AppBarLabPage> {
-  TextEditingController textController;
+  TextEditingController titleController;
+  bool centerTitle = false;
 
   @override
   void initState() {
     super.initState();
-    textController = TextEditingController(text: widget.title);
+    titleController = TextEditingController(text: widget.title);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(textController.value.text),
+        title: Text(titleController.value.text),
+        centerTitle: centerTitle,
       ),
       body: DefaultTextStyle(
-        style: Theme.of(context).textTheme.headline,
+        style: Theme.of(context).textTheme.body1,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Text('text'),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        controller: textController,
-                        onEditingComplete: () {
-                          setState(() {});
-                        },
-                      ),
+              Text('AppBar('),
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text('title:Text(\' '),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              controller: titleController,
+                              onEditingComplete: () {
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                        ),
+                        Text(' \'),')
+                      ],
                     ),
-                  )
-                ],
-              )
+                    Row(
+                      children: <Widget>[
+                        Text('centerTitle: '),
+                        InkWell(
+                          child: Text(
+                            centerTitle.toString(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .body2
+                                .copyWith(color: Colors.red),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              centerTitle = !centerTitle;
+                            });
+                          },
+                        ),
+                        Text(',')
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Text(')'),
             ],
           ),
         ),
